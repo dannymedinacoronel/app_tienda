b62ad0bb4a5f42624e54ee23f0c70175c86906da
Danny Medina Coronel <dannymedinacoronel@gmail.com>
2026-06-12 08:56:35 +0200

    Versión final // APP FINALIZADA DIOS NOS BENDIGA !!! final   final  final iiii añadido gestion de factuas etc... todo emjorado y notaas arregladas y imagenes modo visual tood  areglaod sistema mejorado a full arreglado ahora si ahora sehjbbjbhhjbj

--- server.js
+++ server.js
@@ -137,11 +137,11 @@ app.use(session({
     secret: process.env.SESSION_SECRET || 'clave_maestra_seychelles_987654321',
     resave: false, 
     saveUninitialized: false, 
-    proxy: isProd, // Solo confiar en proxy en producción
+    proxy: true, // Vital para Render/Heroku
     store: MongoStore.create({ mongoUrl: MONGO_URI_FINAL, collectionName: 'sesiones_activas', ttl: 14 * 24 * 60 * 60 }),
     cookie: { 
-        secure: isProd, // True solo en HTTPS
-        sameSite: isProd ? 'none' : 'lax',
+        secure: isProd, 
+        sameSite: isProd ? 'none' : 'lax', // Permite el flujo de Google
         maxAge: 14 * 24 * 60 * 60 * 1000 
     }
 }));
@@ -446,7 +446,7 @@ app.get('/api/ventas', exigeAdmin, async (req, res) => {
         gastosExtra.forEach(g => totalGastosOperativos += g.monto);
 
         const ventas = ventasRaw.map(v => {
-            const proveedorNombre = v.tienda && v.tienda.nombre ? v.tienda.nombre : 'Particular';
+            const proveedorNombre = v.tienda && v.tienda.nombre ? v.tienda.nombre : 'Sin Tienda';
             
             const cant = parseInt(v.cantidad, 10) || 0;
             const pCompra = parseFloat(v.precioCompra) || 0;

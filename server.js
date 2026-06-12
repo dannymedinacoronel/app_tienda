7ecbcb90fe3b6e37a1d55bb0e08c2b427b3af49c
Danny Medina Coronel <dannymedinacoronel@gmail.com>
2026-06-12 09:31:53 +0200

    Versión final // APP FINALIZADA DIOS NOS BENDIGA !!! final   final  final iiii añadido gestion de factuas etc... todo emjorado y notaas arregladas y imagenes modo visual tood  areglaod sistema mejorado a full arreglado ahora si ahora sehjbbjbhhjbjhbkhhkjbdsadsadsa

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET || 'clave_maestra_seychelles_987654321',
     resave: false, 
     saveUninitialized: false, 
-    proxy: isProd, // Solo confiar en proxy en producción
+    proxy: true, // Necesario para que las cookies funcionen tras el balanceador de carga de Render
     store: MongoStore.create({ mongoUrl: MONGO_URI_FINAL, collectionName: 'sesiones_activas', ttl: 14 * 24 * 60 * 60 }),
     cookie: { 
         secure: isProd, 

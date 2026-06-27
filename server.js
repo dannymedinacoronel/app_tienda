ec3483f26a5b5cece7f35a7c9f4421623d7d65d2
Danny Medina Coronel <dannymedinacoronel@gmail.com>
2026-06-27 05:52:07 +0200

    Solucionados los conflictos con la rama principalEDEDdsad dasdr4r4r4f finalmjnaddasdhkjhkkjlj final mejora velocidad etc..  muchas ocsas arregladaspkjij DESTROADP reestructuradaniiubgui reparadoasdsafgdvvdknfdsfdsdsanoiohdasdakoindsdadsdad  tood arrelado dsaindnao arreglado el login y el server caida a trozosykyuy jhjhjh

--- server.js
+++ server.js
@@ -303,7 +303,6 @@ mongoose.connect(MONGO_URI_FINAL || 'mongodb://localhost:27017/seychelles_crm')
         }
     })
     .catch(err => {
-        // Si el error es de clave duplicada durante el seeding, es un comportamiento esperado en reinicios y se puede ignorar para que el servidor no se caiga.
         if (err.code === 11000) {
             console.log('[DB-INFO] Se ignoró un error de clave duplicada durante el seeding inicial. El servidor continuará.');
         } else {
@@ -572,8 +571,7 @@ app.get('/api/auth/verificar', (req, res) => {
                 permisos: permisoDoc.seccionesPermitidas
             });
         }).catch(() => res.json({ autenticado: false }));
-    }
-    else {
+    } else {
         return res.json({ autenticado: false, error: 'No hay sesión activa' });
     }
 });
